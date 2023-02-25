45e84e7763604833d74c49b92c3682e7d42878ea
5UP3R_D1N <42496449+5UP3R-D1N@users.noreply.github.com>
2023-02-25 21:44:16 +0700

    Another update!

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 const { app, BrowserWindow, ipcMain } = require("electron");
 const { Worker } = require("worker_threads");
 const path = require("path");
+const fs = require("fs");
 let services = {};
 
 function createService(name) {
@@ -34,7 +35,6 @@ function createWindow() {
 	mainWindow.loadFile("index.html");
 	mainWindow.on("ready-to-show", mainWindow.maximize);
 	mainWindow.setAutoHideMenuBar(true);
-	createService("relay");
 	// Open the DevTools.
 	// mainWindow.webContents.openDevTools();
 }
@@ -44,7 +44,7 @@ function createWindow() {
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
 	createWindow();
-
+	createService("relay");
 	app.on("activate", function () {
 		// On macOS it's common to re-create a window in the app when the
 		// dock icon is clicked and there are no other windows open.
@@ -55,10 +55,15 @@ app.whenReady().then(() => {
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on("window-all-closed", async function () {
-	require("fs").writeFileSync("./arch_state.bin.zst", services.savedata);
-	if (process.platform !== "darwin") app.quit();
-});
+const allClosed = async function () {
+	try {
+		require("fs").writeFileSync("./arch_state.bin.zst", services.savedata);
+		if (process.platform !== "darwin") app.quit();
+	} catch (e) {
+		createWindow();
+	}
+};
+app.on("window-all-closed", allClosed);
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
@@ -66,3 +71,13 @@ app.on("window-all-closed", async function () {
 ipcMain.on("getSaveMGR", (e, save) => {
 	services.savedata = save;
 });
+ipcMain.on("restart", () => {
+	if (fs.existsSync("arch_state.bin.zst")) fs.unlinkSync("arch_state.bin.zst");
+	app.relaunch();
+	app.quit();
+});
+setInterval(() => {
+	try {
+		require("fs").writeFileSync("./arch_state.bin.zst", services.savedata);
+	} catch (e) {}
+}, 60000);
--- renderer.js
+++ renderer.js
@@ -5,9 +5,20 @@
  * `contextIsolation` is turned on. Use the contextBridge API in `preload.js`
  * to expose Node.js functionality from the main process.
  */
-const { ipcRenderer } = require("electron");
+const { ipcRenderer, BrowserWindow } = require("electron");
+const { unlinkSync } = require("fs");
 var i = "";
 setInterval(async () => {
 	let save = await window.v86savemgr.pe();
 	ipcRenderer.send("getSaveMGR", Buffer.from(save));
 }, 2000);
+
+window.addEventListener(
+	"DOMContentLoaded",
+	() =>
+		(document.getElementById("fixboot").onclick = () => {
+			unlinkSync("arch_state.bin.zst");
+			console.log("Fixing boot...");
+			ipcRenderer.send("restart");
+		})
+);
